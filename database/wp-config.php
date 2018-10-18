1f3c5e4be9c9b6740d882df156d8a94f46c43a89
yamasusano <36954938+yamasusano@users.noreply.github.com>
2018-10-19 01:19:09 +0700

    Update wp-config.php

--- database/wp-config.php
+++ database/wp-config.php
@@ -23,13 +23,13 @@
 define('DB_NAME', 'capstone_project');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'admin');
 
 /** MySQL database password */
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', '150.95.113.196');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
