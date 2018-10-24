a5ff537da1a3884222784636753cf5bffaa88681
Edgy <longttse04476@fpt.edu.vn>
2018-10-24 19:10:37 +0700

    @long111122
    Merge source

--- wp-content/themes/metal-child/validate.php
+++ wp-content/themes/metal-child/validate.php
@@ -1,58 +1,35 @@
 <?php
-
-define('MAX_LENGTH_TITLE', 200);
-define('MAX_LENGTH_DESCRIPTION', 400);
+define("MAX_LENGTH_TITLE",200);
+define("MAX_LENGTH_DESCRIPTION",400);
 function titleCheck($title)
 {
     $title = formatText($title);
     $is_empty = validEmptyField($title);
-    $is_over_length = validMaxLength($title, constant('MAX_LENGTH_TITLE'));
+    $is_over_length = validMaxLength($title, constant("MAX_LENGTH_TITLE"));
     if (!$is_empty['result']) {
-        return array('result' => false, 'message' => 'Title'.$is_empty['message']);
-    } elseif (!$is_over_length['result']) {
-        return array('result' => false, 'message' => 'Title'.$is_over_length['message']);
+        return array('result' => false, 'message' => $is_empty['message']);
+    } else if (!$is_over_length['result']){
+        return array('result' => false, 'message' => $is_over_length['message']);
     }
-
     return array('result' => true);
 }
+
+function formatText($value)
+{
+    return trim($value);
+}
+
 function descriptionCheck($description)
 {
     $description = formatText($description);
     $is_empty = validEmptyField($description);
-    $is_over_length = validMaxLength($description, constant('MAX_LENGTH_DESCRIPTION'));
+    $is_over_length = validMaxLength($description, constant("MAX_LENGTH_DESCRIPTION"));
     if (!$is_empty['result']) {
-        return array('result' => false, 'message' => 'Description'.$is_empty['message']);
-    } elseif (!$is_over_length['result']) {
-        return array('result' => false, 'message' => 'Description'.$is_over_length['message']);
+        return array('result' => false, 'message' => $is_empty['message']);
+    } else if (!$is_over_length['result']){
+        return array('result' => false, 'message' => $is_over_length['message']);
     }
-
     return array('result' => true);
-}
-function closeDateCheck($close_date)
-{
-    $deadline = getDeadLine();
-    $is_validated_time = checkCloseDate($close_date, $deadline);
-    if (!$is_validated_time['result']) {
-        return array('result' => false, 'message' => 'Date to close form '.$is_validated_time['message']);
-    }
-
-    return array('result' => true);
-}
-
-function checkCloseDate($close_date, $deadline)
-{
-    $date_format = '%H:%M:%S %d-%B-%Y';
-    $date_format_result = '%d-%B-%Y';
-    $deadline_result = strftime($date_format_result, $deadline);
-    $today = strftime($date_format);
-    $close_date = strftime($date_format, $close_date);
-    $deadline = strftime($date_format, $deadline);
-    if (strtotime($close_date) <= strtotime($deadline) &&
-        strtotime($close_date) - strtotime($today) >= 48 * 60 * 60) {
-        return array('result' => true);
-    }
-
-    return array('result' => false, 'message' => ' is before '.$deadline_result.' and after 48h from when the form opens.');
 }
 
 function validEmptyField($value)
@@ -60,8 +37,7 @@ function validEmptyField($value)
     if (!empty($value)) {
         return array('result' => true);
     }
-
-    return array('result' => false, 'message' => ' cannot empty. Please try again!!!');
+    return array('result' => false, 'message' => ' field cannot empty. Please try again!!!');
 }
 
 function validMaxLength($value, $length)
@@ -69,43 +45,30 @@ function validMaxLength($value, $length)
     if (strlen($value) <= $length) {
         return array('result' => true);
     }
-
-    return array('result' => false, 'message' => ' is not over '.$length.' characters.');
+    return array('result' => false, 'message' => ' field is not over '.$length.' characters');
 }
 
-function formatText($value)
+function closeDateCheck($close_date)
 {
-    return trim($value);
-}
-function validFormFinder($title, $description, $close_date)
-{
-    $message = '';
-    $deadline = getDeadLine();
-    if (!titleCheck($title)['result']) {
-        $message = titleCheck($title)['message'];
-
-        return $message;
-    } elseif (!descriptionCheck($description)['result']) {
-        $message = descriptionCheck($description)['message'];
-
-        return $message;
-    } elseif (!closeDateCheck($close_date)['result']) {
-        $message = closeDateCheck($close_date)['message'];
-
-        return $message;
-    } else {
-        return true;
+    $deadline = "10-09-2018";
+    $is_validated_time = validCloseDate($close_date, $deadline);
+    if (!$is_validated_time['result']) {
+        return array('result' => false, 'message' => $is_validated_time['message']);
     }
+    return array('result' => true);
 }
-function getDeadLine()
+
+function checkCloseDate($close_date, $deadline)
 {
-    global $wpdb;
-
-    $deadline = $wpdb->get_var("
-    SELECT end 
-    FROM {$wpdb->prefix}semester 
-    WHERE status = 1
-    ");
-
-    return $dead_line;
+    $date_format = "%H:%M:%S %d/%B/%Y";
+    $date_format_result = "%d-%B-%Y";
+    $deadline_result = strftime($date_format_result, $deadline);
+    $today = strftime($date_format);
+    $close_date = strftime($date_format, $close_date);
+    $deadline = strftime($date_format, $deadline);
+    if (strtotime($close_date) <= strtotime($deadline) &&
+        strtotime($close_date) - strtotime($today) >= 48*60*60) {
+        return array('result' => true);
+    }
+    return array('result' => false, 'message' => ' is before '.$deadline_result.' and after 48h from when the form opens');
 }
